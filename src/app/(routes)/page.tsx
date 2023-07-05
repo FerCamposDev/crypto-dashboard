225204a570fcab116870b340ca980aeb8f4c9f71
fercamposdev <fernando.developments@gmail.com>
2023-07-04 23:08:19 -0300

    eslint config

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,30 +1,28 @@
 'use client';
-import { useEffect, useState } from "react";
-import { fetchBalance, getNetwork, FetchBalanceResult } from '@wagmi/core'
-import { avalanche, bsc, mainnet, goerli } from '@wagmi/core/chains'
-import useTokenBalance from "@/hooks/useTokenBalance";
+import { goerli } from '@wagmi/core/chains';
+import useTokenBalance from '@/hooks/useTokenBalance';
 
 const EUROC_CONTRACT = '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c';
 
-const ADDRESS = '0x52b6780bd3D62dAd028475f13da8DA7bc5D73aE6'
+const ADDRESS = '0x52b6780bd3D62dAd028475f13da8DA7bc5D73aE6';
 
 export default function Home() {
-  const euroc = useTokenBalance({
-    address: ADDRESS,
-    token: EUROC_CONTRACT,
-  });
-  const goerliBalance = useTokenBalance({
-    address: ADDRESS,
-    chainId: goerli.id
-  });
+	const euroc = useTokenBalance({
+		address: ADDRESS,
+		token: EUROC_CONTRACT,
+	});
+	const goerliBalance = useTokenBalance({
+		address: ADDRESS,
+		chainId: goerli.id
+	});
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
       Main page
-      <div>
-        <p>EUROC balance: {euroc.isFetching ? 'loading' : `${euroc?.data?.formatted} ${euroc.data?.symbol}`}</p>
-        <p>gETH balance: {goerliBalance.isFetching ? 'loading' : `${goerliBalance?.data?.formatted} ${goerliBalance.data?.symbol}`}</p>
-      </div>
-    </main>
-  )
+			<div>
+				<p>EUROC balance: {euroc.isFetching ? 'loading' : `${euroc?.data?.formatted} ${euroc.data?.symbol}`}</p>
+				<p>gETH balance: {goerliBalance.isFetching ? 'loading' : `${goerliBalance?.data?.formatted} ${goerliBalance.data?.symbol}`}</p>
+			</div>
+		</main>
+	);
 }
--- src/hooks/useTokenBalance.ts
+++ src/hooks/useTokenBalance.ts
@@ -1,17 +1,17 @@
 import {
-  useQuery,
-} from '@tanstack/react-query'
-import { fetchBalance, Address, FetchBalanceArgs, FetchBalanceResult } from '@wagmi/core'
+	useQuery,
+} from '@tanstack/react-query';
+import { fetchBalance, FetchBalanceArgs } from '@wagmi/core';
 
 const useTokenBalance = (props: FetchBalanceArgs) => {
-  const { address, token } = props;
-  return useQuery({
-    queryKey: ['token', address, token],
-    queryFn: () => fetchBalance({
-      address,
-      token,
-    })
-  })
-}
+	const { address, token } = props;
+	return useQuery({
+		queryKey: ['token', address, token],
+		queryFn: () => fetchBalance({
+			address,
+			token,
+		})
+	});
+};
 
 export default useTokenBalance;
